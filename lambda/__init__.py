d6593d387abebc003d338426c8d94b08279bc998
Emma Paris <l.baroffio@gmail.com>
2016-04-22 17:24:15 +0200

    add init file

---
+++ lambda/__init__.py
@@ -0,0 +1,8 @@
+import os
+import ctypes
+
+for d, dirs, files in os.walk('lib'):
+    for f in files:
+        if f.endswith('.a'):
+            continue
+        ctypes.cdll.LoadLibrary(os.path.join(d, f))
